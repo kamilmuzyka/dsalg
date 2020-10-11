8bea67aba2a5d2d470db5c3917fdf0aa1635d008
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-10-11 23:41:32 +0100

    Fix logical errors in the Stack ADT

--- index.js
+++ index.js
@@ -5,31 +5,33 @@ class Stack {
         this.top = 0
     }
 
+    empty() {
+        return this.top === 0
+    }
+
+    full() {
+        return this.top === this.size
+    }
+
     push(element) {
-        if(this.storage.length < this.size) {
-            this.storage.push(element)
+        if(this.top < this.size) {
+            this.storage[this.top] = element
+            this.top = this.top + 1
         } else {
             throw new Error('Exception: Unable to execute push operation on a full stack')
         }
     }
 
     pop() {
-        if(this.storage.length > 0) {
+        if(this.empty() === false) {
             this.storage.pop()
+            this.top = this.top - 1
         } else {
             throw new Error('Exception: Unable to execute pop operation on an empty stack')
         }
     }
 
     peek() {
-        return this.storage[this.storage.length - 1]
-    }
-
-    empty() {
-        return this.storage.length === 0 ? true : false
-    }
-
-    full() {
-        return this.storage.length === this.size ? true : false
+        return this.storage[this.top - 1]
     }
 }
