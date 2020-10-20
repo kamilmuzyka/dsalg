3558b00a2b97a7cc23a55dec6b895a60ea63da9a
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-10-20 13:55:50 +0100

    Add the initially sorted array case to the Bubble Sort algorithm

--- algorithms/sorting/bubble.js
+++ algorithms/sorting/bubble.js
@@ -1,10 +1,13 @@
 function bubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
+        let sorted = true;
         for (let j = 0; j < array.length - 1; j++) {
             if (array[j] > array[j + 1]) {
                 [array[j], array[j + 1]] = [array[j + 1], array[j]];
+                sorted = false;
             }
         }
+        if (sorted) break;
     }
 }
 
