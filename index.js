7d762cbea0310891183349e26625b269025e65e0
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-10-27 16:48:55 +0000

    Recursion - digitalSum

--- index.js
+++ index.js
@@ -9,5 +9,3 @@ const {
 const selectionSort = require('./algorithms/sorting/selection');
 const bubbleSort = require('./algorithms/sorting/bubble');
 const insertionSort = require('./algorithms/sorting/insertion');
-const factorial = require('./algorithms/recursive/factorial');
-const fibonacci = require('./algorithms/recursive/fibonacci');
---
+++ tasks/factorial.js
@@ -0,0 +1,10 @@
+// The factorial of a positive number n, denoted by n! is defined as the product of the integers from 1 to n. Design and write a recursive algorithm that implements the factorial(n). For example, factorial(5) = 5 * 4 * 3 * 2 * 1 = 120
+
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+module.exports = factorial;
