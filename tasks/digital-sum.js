829c1affd322492ded72693f24062ad216c798d9
Kamil Muzyka <kamil.muzyka@gmail.com>
2020-10-27 16:50:33 +0000

    Break comments

--- tasks/digital-sum.js
+++ tasks/digital-sum.js
@@ -1,4 +1,7 @@
-// The digital sum of a number n is the sum of its digits. Design and write a recursive algorithm that takes a positive integer n and returns its digital sum. For example, digitalSum(2019) = 12, because 2 + 0 + 1 + 9 = 12
+// The digital sum of a number n is the sum of its digits.
+// Design and write a recursive algorithm
+// that takes a positive integer n and returns its digital sum.
+// For example, digitalSum(2019) = 12, because 2 + 0 + 1 + 9 = 12
 
 function digitalSum(n, m) {
     const array = [...String(n)];
--- tasks/fibonacci.js
+++ tasks/fibonacci.js
@@ -1,4 +1,6 @@
-// The Fibonacci numbers are 1, 1, 2, 3, 5, 8, 13, 21... The nth Fibonacci number is the sum of the previous two Fibonacci numbers. Design and write a recursive algorithm that determines the nth Fibonacci number.
+// The Fibonacci numbers are 1, 1, 2, 3, 5, 8, 13, 21...
+// The nth Fibonacci number is the sum of the previous two Fibonacci numbers.
+// Design and write a recursive algorithm that determines the nth Fibonacci number.
 
 function fibonacci(n) {
     if (n === 1 || n === 2)  {
