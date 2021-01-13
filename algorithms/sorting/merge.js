2430f57831a85e558aa37d7ebda2474b9999d62c
kamilmuzyka <kamil.muzyka@gmail.com>
2021-01-13 23:08:15 +0100

    Tidy up

--- algorithms/sorting/merge.js
+++ algorithms/sorting/merge.js
@@ -1,3 +1,17 @@
+function merge(left, right) {
+    const array = [];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            const target = left.shift();
+            array.push(target);
+        } else {
+            const target = right.shift();
+            array.push(target);
+        }
+    }
+    return [...array, ...left, ...right];
+}
+
 function mergeSort(array) {
     if (array.length < 2) {
         return array;
@@ -5,19 +19,6 @@ function mergeSort(array) {
     const midpoint = Math.floor(array.length / 2);
     const left = array.slice(0, midpoint);
     const right = array.slice(midpoint);
-    const merge = (left, right) => {
-        const array = [];
-        while (left.length && right.length) {
-            if (left[0] < right[0]) {
-                const target = left.shift();
-                array.push(target);
-            } else {
-                const target = right.shift();
-                array.push(target);
-            }
-        }
-        return [...array, ...left, ...right];
-    };
     return merge(mergeSort(left), mergeSort(right));
 }
 
--- algorithms/sorting/quick.js
+++ algorithms/sorting/quick.js
@@ -1,19 +1,20 @@
+function partition(array, left, right) {
+    const pivot = array[right];
+    let i = left - 1;
+    for (let j = left; j <= right - 1; j++) {
+        if (array[j] < pivot) {
+            i += 1;
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+    [array[i + 1], array[right]] = [array[right], array[i + 1]];
+    return i + 1;
+}
+
 function quickSort(array, left = 0, right = array.length - 1) {
     if (left >= right) {
         return;
     }
-    const partition = (array, left, right) => {
-        const pivot = array[right];
-        let i = left - 1;
-        for (let j = left; j <= right - 1; j++) {
-            if (array[j] < pivot) {
-                i += 1;
-                [array[i], array[j]] = [array[j], array[i]];
-            }
-        }
-        [array[i + 1], array[right]] = [array[right], array[i + 1]];
-        return i + 1;
-    };
     const pivot = partition(array, left, right);
     quickSort(array, left, pivot - 1);
     quickSort(array, pivot + 1, right);
