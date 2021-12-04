6fafc74491c58b6ab0e77ac12bdc28eff0e517de
kamilmuzyka <kamil.muzyka@gmail.com>
2021-12-04 00:03:34 +0000

    isValidSubsequence

--- questions.js
+++ questions.js
@@ -73,3 +73,25 @@ function twoNumberSum2(array, target) {
  *  x - array item
  *  y - ?
  *  z - target */
+
+/** 5. Given two non-empty arrays of integers, write a function that determines
+ * whether the second array is a subsequence of the first one. A subsequence of
+ * an array is a set of numbers that aren't necessarily adjacent in the array
+ * but that are in the same order as they appear in the array. For instance, the
+ * numbers [1, 3, 4] form a subsequence of the array [1, 2, 3, 4], and so do the
+ * numbers [2, 4]. Note that a single number in an array and the array itself
+ * are both valid subsequences of the array. */
+
+/** O(1) Space and O(n) Time */
+function isValidSubsequence(array, sequence) {
+    let next = 0;
+    for (const number of array) {
+        if (number === sequence[next]) {
+            next++;
+        }
+        if (next === sequence.length) {
+            return true;
+        }
+    }
+    return false;
+}
