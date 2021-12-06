82e6d741f208979fce2465f8cf9fcc9f88cb2908
kamilmuzyka <kamil.muzyka@gmail.com>
2021-12-06 20:37:38 +0000

    isPalindrome

--- questions.js
+++ questions.js
@@ -250,3 +250,23 @@ class BST {
         return current.value;
     }
 }
+
+/** 9. Write a function that takes in a non-empty string and that returns a boolean
+ * representing whether the string is a palindrome. A palindrome is defined as a
+ * string that's written the same forward and backward. Note that
+ * single-character strings are palindromes. */
+
+/** O(1) Space | O(n) Time */
+function isPalindrome(string) {
+    let left = 0;
+    let right = string.length - 1;
+    while (left < right) {
+        if (string[left] === string[right]) {
+            left++;
+            right--;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
