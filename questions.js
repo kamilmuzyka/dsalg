f62867e815bf9ced6b952796b81c2ada9f26ea6b
kamilmuzyka <kamil.muzyka@gmail.com>
2021-12-07 20:00:58 +0000

    removeDuplicatesFromLinkedList

--- questions.js
+++ questions.js
@@ -301,3 +301,27 @@ function isPalindrome(string) {
     }
     return true;
 }
+
+/** 10. You're given the head of a Singly Linked List whose nodes are in sorted
+ * order with respect to their values. Write a function that returns a modified
+ * version of the Linked List that doesn't contain any nodes with duplicate
+ * values. The Linked List should be modified in place (i.e., you shouldn't
+ * create a brand new list), and the modified Linked List should still have its
+ * nodes sorted with respect to their values. Each LinkedList node has an
+ * integer value as well as a next property pointing to the next node in the
+ * list or to null if it's the tail of the list. */
+
+/** O(1) Space | O(n) Time */
+function removeDuplicatesFromLinkedList(linkedList) {
+    let current = linkedList;
+    let previous = null;
+    while (current) {
+        if (previous?.value === current.value) {
+            previous.next = current.next;
+        } else {
+            previous = current;
+        }
+        current = current.next;
+    }
+    return linkedList;
+}
