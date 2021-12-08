91478aef676e87039b9deed6a855dd6f165c0b76
kamilmuzyka <kamil.muzyka@gmail.com>
2021-12-08 19:43:07 +0000

    firstNonRepeatingCharacter

--- questions.js
+++ questions.js
@@ -325,3 +325,23 @@ function removeDuplicatesFromLinkedList(linkedList) {
     }
     return linkedList;
 }
+
+/** 11. Write a function that takes in a string of lowercase English-alphabet
+ * letters and returns the index of the string's first non-repeating character.
+ * The first non-repeating character is the first character in a string that
+ * occurs only once. If the input string doesn't have any non-repeating
+ * characters, your function should return -1. */
+
+/** O(1) Space | O(n) Time */
+function firstNonRepeatingCharacter(string) {
+    const characters = {};
+    for (character of string) {
+        characters[character] = character in characters;
+    }
+    for (character in characters) {
+        if (characters[character] === false) {
+            return string.indexOf(character);
+        }
+    }
+    return -1;
+}
